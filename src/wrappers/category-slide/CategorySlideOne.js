21b2d8bbe7bf7d40966d281fc6548a3d0ca6baa1
Nguyen Duc Thanh <thanh.nguyen@beetech.com>
2024-06-26 18:09:34 +0700

    handle preload logo

--- src/wrappers/category-slide/CategorySlideOne.js
+++ src/wrappers/category-slide/CategorySlideOne.js
@@ -14,7 +14,6 @@ const CategorySlideOne = () => {
   return (
     <Fragment>
       <div
-        className="product-area"
         style={{ marginTop: "3rem", marginBottom: "3rem" }}
       >
         <Box
